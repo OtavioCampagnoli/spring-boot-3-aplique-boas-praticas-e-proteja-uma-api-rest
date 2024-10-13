2902a35964b9dce7ba6b54ad2faecbe8fbe53706
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-13 15:32:26 -0300

    Tratando erro 400

--- backend/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ backend/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package med.voll.api.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,19 @@ public class TratadorDeErros {
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity tratarError404() {
 		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarError400(MethodArgumentNotValidException ex) {
+		
+		var error = ex.getFieldErrors();
+		
+		return ResponseEntity.badRequest().body(error.stream().map(DadosErroValidacao::new).toList());
+	}
+	
+	private record DadosErroValidacao(String campo, String mensagem) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
 	}
 }
