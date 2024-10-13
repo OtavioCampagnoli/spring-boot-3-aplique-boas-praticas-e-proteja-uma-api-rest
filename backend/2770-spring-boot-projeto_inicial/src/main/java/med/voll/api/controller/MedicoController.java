e1e8c04977591028fb6a0eb4c0a2db7666c41a5c
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-13 13:04:19 -0300

    Detalhando dados na API

--- backend/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/controller/MedicoController.java
+++ backend/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,28 @@
 package med.voll.api.controller;
 
-import jakarta.validation.Valid;
-import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDetalhamentoDoMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
@@ -50,6 +63,13 @@ public class MedicoController {
         var medico = repository.getReferenceById(id);
         medico.excluir();   
         return ResponseEntity.noContent().build();
+        
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoDoMedico(medico));
         
     }
 
