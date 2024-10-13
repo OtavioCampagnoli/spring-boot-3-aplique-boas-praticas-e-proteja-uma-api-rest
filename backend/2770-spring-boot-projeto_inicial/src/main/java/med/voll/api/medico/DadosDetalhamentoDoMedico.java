bbba75e15fc9b73b66815bbe82293703035d96db
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-10-13 15:10:15 -0300

    Tratando erro 404

--- backend/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/medico/DadosDetalhamentoDoMedico.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.medico;
-
-import med.voll.api.endereco.Endereco;
-
-public record DadosDetalhamentoDoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
-	public DadosDetalhamentoDoMedico(Medico medico) {
-		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
-	}
-}
